f2510ba2adb0c2797d16b3a31ab7d96de59cd0f3
Manoj81-star <manojmanu4156@gmail.com>
2026-05-07 14:40:31 +0530

    Day 3  — Create and edit form with client side validation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
-
-  Setup (Day 1):
-    npm create vite@latest frontend -- --template react
+ite@latest frontend -- --template react
     cd frontend
     npm install axios recharts react-router-dom date-fns
     npm install -D tailwindcss postcss autoprefixer
